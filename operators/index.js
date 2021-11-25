67f162d993d9802b0b7bf8da87bc578f6079aae3
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-11-25 18:46:38 +0200

    Exercises with operators of assignment, comparison, and logical and ternary operators.

--- operators/index.js
+++ operators/index.js
@@ -156,3 +156,112 @@ console.log('undefined == 0:', undefined == 0);
 console.log('null == 0:', null == 0);
 console.log('undefined < 0:', undefined < 0);
 console.log('undefined > 0:', undefined > 0);
+
+/* eslint-disable */
+
+/* оператор || (OR) */
+false || true;
+false || true || false;
+false || false || false;
+console.log("false || true:", false || true);
+console.log("false || true || false:", false || true || false);
+console.log("false || false || false:", false || false || false);
+
+/* оператор && (AND) */
+false && true;
+false && true && false;
+true && true && true;
+console.log("false && true:", false && true);
+console.log("false && true && false:", false && true && false);
+console.log("true && true && true:", true && true && true);
+
+/* Логические операторы могут применятся к любым типам данных */
+
+/* || находит первое истинное значение */
+'text' || false;
+true || 'text';
+undefined || null || 0 || '' || null;
+console.log("\'text\' || false:", 'text' || false);
+console.log("true || \'text\':", true || 'text');
+console.log("undefined || null || 0 || '' || null:",undefined || null || 0 || '' || false);
+
+const customAmount = null; // не выводим
+const defaultAmount = 17; // не выводим
+const amount = customAmount || defaultAmount; // выводим значение переменной amount в формате console.log('amount:', amount);
+console.log('amount:', amount);
+
+/* && находит первое ложное значение */
+'text' && false;
+true && 'text';
+undefined && null && 0 && '' && null;
+console.log("'text' && false:", 'text' && false);
+console.log("true && 'text':", true && 'text');
+console.log("undefined && null && 0 && '' && null:", undefined && null && 0 && '' && null);
+
+/* оператор отрицания (OR) */
+!true;
+!false;
+!'text';
+!!'text';
+console.log("!true:", !true);
+console.log("!false:", !false);
+console.log("!'text':", !'text');
+console.log("!!'text':", !!'text');
+
+/* eslint-disable */
+
+if (true) console.log("I'm in");
+
+if (false) {
+  console.log("I'm not in");
+}
+
+if ('text') {
+  console.log('Yes');
+}
+
+if (null) {
+  console.log('Yes');
+} else {
+  console.log('Else');
+}
+
+// create age variable here
+
+const age = 19;
+
+if (age < 16) {
+  console.log('Too young');
+} else if (age >= 16 && age < 18) {
+  console.log('Almost ready');
+} else if (age >= 18 && age < 65) {
+  console.log('Ready');
+} else {
+  console.log('Else');
+}
+
+// Тернаный оператор ? : ( const result = условие ? значение1 : значение2; )
+
+// create amount variable here
+
+const amount = 16;
+
+const result = amount > 17 ? 'full' : 'empty';
+
+/* Этот код менять не нужно */
+const defaultUserName = 'anonymus';
+const userName = 'John';
+const anotherUser = null;
+const isAdmin = false;
+const message = 'text';
+const anotherMessage = 'text as well';
+const isLoggedIn = Boolean(userName);
+
+/* В коде ниже нужно использовать правильные операторы после знака присваивания */
+const activeUser = anotherUser || defaultUserName;
+const hasAccess = isAdmin || isLoggedIn;
+const isTruthy = message < anotherMessage;
+const isFalsy = !isLoggedIn;
+const isNotTrue = typeof message != typeof 'number';
+const isTrue = typeof message == typeof 'boolean';
+console.log(isTrue);
