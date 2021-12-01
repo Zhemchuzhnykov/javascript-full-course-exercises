7108ba5826fecb401beb8c4243f293a5731ef791
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-01 15:22:55 +0200

    Typing errors specified by Eslint in a text document

--- loops/script.js
+++ loops/script.js
@@ -1,25 +1,25 @@
-const n = 119;
-let sum = 0;
-for (let i = 1; i <= n; i++) {
-  sum += i;
-}
-console.log('Result: ' + sum);
+// const n = 119;
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log('Result: ' + sum);
 
-const m = 1;
-const n = 19;
-let result = 1;
-for (let i = 1; i <= 19; i++) {
-  if (i % 2 !== 0) {
-    result *= i;
-  }
-}
-console.log(result);
+// const m = 1;
+// const n = 19;
+// let result = 1;
+// for (let i = 1; i <= 19; i++) {
+//   if (i % 2 !== 0) {
+//     result *= i;
+//   }
+// }
+// console.log(result);
 
-for (let i = 1; i <= 5; i++) {
-  let result = 0;
-  console.log("\n=> " + i + " <=\n\n");
-  for (let n = 1; n < 10; n++) {
-    result = i * n;
-    console.log(i + " x " + n + " = " + result);
-  }
-}
+// for (let i = 1; i <= 5; i++) {
+//   let result = 0;
+//   console.log("\n=> " + i + " <=\n\n");
+//   for (let n = 1; n < 10; n++) {
+//     result = i * n;
+//     console.log(i + " x " + n + " = " + result);
+//   }
+// }
