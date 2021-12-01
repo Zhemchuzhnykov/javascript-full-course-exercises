be240cf8a6c318d0da6edec17b6f28a698807ecc
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-01 16:52:08 +0200

    Exercise with counting a sum of odds numbers in a range.

--- loops/script.js
+++ loops/script.js
@@ -55,3 +55,18 @@ for (let i = 1; i <= end; i++) {
     result *= i;
   } else {}
 }
+
+// exercise 6
+
+let sum = 0;
+for (let i = 0; i <= 1000; i++) {
+  if (i % 2 !== 0) {
+    console.log("Found");
+    sum += i;
+  }
+}
+if (sum * 5 > 5000) {
+  console.log("Bigger");
+} else {
+  console.log("Smaller or equal");
+}
