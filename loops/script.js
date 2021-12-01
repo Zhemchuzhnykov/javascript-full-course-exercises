6dc284c68b53c56a82afa4a39b2e7fa80d7887be
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-01 15:57:24 +0200

    Making a string with all numbers in a range.

--- loops/script.js
+++ loops/script.js
@@ -1,25 +1,40 @@
-// const n = 119;
-// let sum = 0;
-// for (let i = 1; i <= n; i++) {
-//   sum += i;
-// }
-// console.log('Result: ' + sum);
+// exercise 1
 
-// const m = 1;
-// const n = 19;
-// let result = 1;
-// for (let i = 1; i <= 19; i++) {
-//   if (i % 2 !== 0) {
-//     result *= i;
-//   }
-// }
-// console.log(result);
+const n = 119;
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+console.log('Result: ' + sum);
 
-// for (let i = 1; i <= 5; i++) {
-//   let result = 0;
-//   console.log("\n=> " + i + " <=\n\n");
-//   for (let n = 1; n < 10; n++) {
-//     result = i * n;
-//     console.log(i + " x " + n + " = " + result);
-//   }
-// }
+// exercise 2
+
+const m = 1;
+const n = 19;
+let result = 1;
+for (let i = 1; i <= 19; i++) {
+  if (i % 2 !== 0) {
+    result *= i;
+  }
+}
+console.log(result);
+
+// exercise 3
+
+for (let i = 1; i <= 5; i++) {
+  let result = 0;
+  console.log("\n=> " + i + " <=\n\n");
+  for (let n = 1; n < 10; n++) {
+    result = i * n;
+    console.log(i + " x " + n + " = " + result);
+  }
+}
+
+// exercise 4
+
+const n = 19;
+let sum = "";
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+console.log(sum);
