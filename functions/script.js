1f1f3068b2b190f82158219627d2ae8b79903875
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-03 23:02:10 +0200

    Creating a function that returns an increased argument that is increased if a certain condition takes place.

--- functions/script.js
+++ functions/script.js
@@ -1,10 +1,16 @@
+// Exercise 1
+
 function sum(num1, num2) {
   return num1 + num2;
 }
 
+// exercise 2
+
 function sayHi(name) {
   console.log("Hi, " + name);
 }
+
+// exercise 3
 
 function getEvenOdd(num) {
   if (num % 2 === 0) {
@@ -13,9 +19,13 @@ function getEvenOdd(num) {
   return "odd";
 }
 
+// exercise 4
+
 function square(num) {
   return num ** 2;
 }
+
+// exercise 5
 
 function getSum(num1, num2) {
   let sum = 0;
@@ -23,4 +33,11 @@ function getSum(num1, num2) {
     if (i % 2 === 0) sum += i;
   }
   return sum;
+}
+
+// exercise 6
+
+function increaser(a, index) {
+  if (a > index) a += index;
+  return a;
 }
