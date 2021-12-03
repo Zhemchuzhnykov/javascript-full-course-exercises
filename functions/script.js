136d1e6652eeb9e6595a6502f41b99d11dcd5226
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-03 23:35:14 +0200

    Creating a function that returns a quantity of numbers in a range of numbers which can be divided on some certain number without a residue.

--- functions/script.js
+++ functions/script.js
@@ -60,3 +60,10 @@ function compareSums(a, b, c, d) {
 
 // exercise 8
 
+function findDivCount(a, b, n) {
+  let count = 0;
+  for (let i = a; i <= b; i++) {
+    if (i % n === 0) count++;
+  }
+  return count;
+}
