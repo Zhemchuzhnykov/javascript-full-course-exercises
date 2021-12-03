5c5dd8242eb957ff174f7de7994d8cbd8317d731
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-03 22:53:46 +0200

    Creating a function returning a sum of all even numbers in a range.

--- functions/script.js
+++ functions/script.js
@@ -16,3 +16,11 @@ function getEvenOdd(num) {
 function square(num) {
   return num ** 2;
 }
+
+function getSum(num1, num2) {
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    if (i % 2 === 0) sum += i;
+  }
+  return sum;
+}
