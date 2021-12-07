4234f923047fa63b5c368803d2e729a407007a66
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2021-12-07 10:40:46 +0200

    Get special numbers function

---
+++ array-methods/script.js
@@ -0,0 +1,4 @@
+// function 1: arrow function => applying filter() to a received array =>
+// building the code returning true for the desired elements at their iterations
+
+const getSpecialNumbers = array => array.filter(element => element % 3 === 0);
