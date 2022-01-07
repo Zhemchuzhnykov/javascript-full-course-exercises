a8111b45060c8272b5ca499fb5624b138a40b03f
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2022-01-07 23:48:00 +0200

    Calculator creator function, unit-tests for the calculator creator.

--- lexical-environment-closure/index.js
+++ lexical-environment-closure/index.js
@@ -7,3 +7,34 @@ export const makeCounter = () => {
 
 const counter1 = makeCounter();
 const counter2 = makeCounter();
+
+export const createCalculator = () => {
+  let number = 0;
+
+  const add = num => {
+    number += num;
+  };
+
+  const decrease = num => {
+    number -= num;
+  };
+
+  const reset = () => {
+    number = 0;
+  };
+
+  const getMemo = () => {
+    return number;
+  };
+
+  return {
+    add,
+    decrease,
+    reset,
+    getMemo,
+  };
+};
+
+const calculator1 = createCalculator();
+const calculator2 = createCalculator();
+
--- lexical-environment-closure/index.test.js
+++ lexical-environment-closure/index.test.js
@@ -1,5 +1,7 @@
 import { it } from 'eslint/lib/rule-tester/rule-tester';
-import { makeCounter } from './index';
+import { makeCounter, createCalculator } from './index';
+
+// counter tests 
 
 it('should return 1 at the second call of the counter', () => {
   const counter1 = makeCounter();
@@ -14,6 +16,43 @@ it('should not return 1 at the second call of the counter', () => {
   const counter2 = makeCounter();
 
   counter1();
-  
+
   expect(counter2()).not.toEqual(1);
 })
+
+// calculator tests
+
+it('should return 10 for the first calculator', () => {
+  const calculator1 = createCalculator();
+
+  calculator1.add(10);
+
+  expect(calculator1.getMemo()).toEqual(10);
+});
+
+it('should return -5 for the first calculator', () => {
+  const calculator1 = createCalculator();
+
+  calculator1.decrease(5);
+
+  expect(calculator1.getMemo()).toEqual(-5);
+});
+
+it('should return 0 for the first calculator', () => {
+  const calculator1 = createCalculator();
+
+  calculator1.decrease(5);
+  calculator1.reset();
+
+  expect(calculator1.getMemo()).toEqual(0);
+});
+
+it('should return 10 for the first calculator and 0 for the second calculator', () => {
+  const calculator1 = createCalculator();
+  const calculator2 = createCalculator();
+
+  calculator1.add(10);
+
+  expect(calculator1.getMemo()).toEqual(10);
+  expect(calculator2.getMemo()).toEqual(0);
+});
