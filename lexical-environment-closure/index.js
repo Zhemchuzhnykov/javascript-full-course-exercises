a92e5305e684bfc468393a4203edfec19b627ed5
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2022-01-08 14:24:06 +0200

    Logger function, and unit tests for this function

--- lexical-environment-closure/index.js
+++ lexical-environment-closure/index.js
@@ -1,3 +1,5 @@
+// counter
+
 export const makeCounter = () => {
   let count = 0;
   return function() {
@@ -7,6 +9,8 @@ export const makeCounter = () => {
 
 const counter1 = makeCounter();
 const counter2 = makeCounter();
+
+// calculator
 
 export const createCalculator = () => {
   let number = 0;
@@ -38,3 +42,49 @@ export const createCalculator = () => {
 const calculator1 = createCalculator();
 const calculator2 = createCalculator();
 
+// logger
+
+export const createLogger = () => {
+
+  const records = []
+
+  const warn = text => {
+    records.push({
+      message: text,
+      type: 'warn',
+      dateTime: new Date(),
+    });
+  };
+
+  const error = text => {
+    records.push({
+      message: text,
+      type: 'error',
+      dateTime: new Date(),
+    });
+  };
+
+  const log = text => {
+    records.push({
+      message: text,
+      type: 'log',
+      dateTime: new Date(),
+    });
+  };
+
+  const getRecords = messageType => {
+    if (messageType === undefined) return records.sort((a, b) => b.dateTime - a.dateTime);
+
+    return records.filter(record => record.type === messageType).sort((a, b) => b.dateTime - a.dateTime);
+  };
+
+  return {
+    warn,
+    error,
+    log,
+    getRecords
+  };
+};
+
+const logger1 = createLogger();
+const logger2 = createLogger();
--- lexical-environment-closure/index.test.js
+++ lexical-environment-closure/index.test.js
@@ -1,5 +1,5 @@
 import { it } from 'eslint/lib/rule-tester/rule-tester';
-import { makeCounter, createCalculator } from './index';
+import { makeCounter, createCalculator, createLogger } from './index';
 
 // counter tests 
 
@@ -56,3 +56,76 @@ it('should return 10 for the first calculator and 0 for the second calculator', 
   expect(calculator1.getMemo()).toEqual(10);
   expect(calculator2.getMemo()).toEqual(0);
 });
+
+// logger tests 
+
+it('should return an object with the value "warn" for the key "type"', () => {
+  const logger = createLogger();
+
+  logger.warn('Cyber-criminals are hacking the system');
+  logger.error('Your girlfriend tried to create a new user for Windows');
+  logger.log('You opened World of Tanks, have a great time!');
+
+  const extractedObject = logger.getRecords('warn');
+
+  expect(extractedObject[0].type).toEqual('warn');
+});
+
+it('should return an object with the value "error" for the key "type"', () => {
+  const logger = createLogger();
+
+  logger.warn('Cyber-criminals are hacking the system');
+  logger.error('Your girlfriend tried to create a new user for Windows');
+  logger.log('You opened World of Tanks, have a great time!');
+
+  const extractedObjects = logger.getRecords('error');
+
+  expect(extractedObjects[0].type).toEqual('error');
+});
+
+it('should return an object with the value "log" for the key "type"', () => {
+  const logger = createLogger();
+
+  logger.warn('Cyber-criminals are hacking the system');
+  logger.error('Your girlfriend tried to create a new user for Windows');
+  logger.log('You opened World of Tanks, have a great time!');
+
+  const extractedObjects = logger.getRecords('log');
+
+  expect(extractedObjects[0].type).toEqual('log');
+});
+
+it('should return three objects of different types', () => {
+  const logger = createLogger();
+
+  logger.warn('Cyber-criminals are hacking the system');
+  logger.error('Your girlfriend tried to create a new user for Windows');
+  logger.log('You opened World of Tanks, have a great time!');
+
+  const extractedObjects = logger.getRecords();
+
+  expect(extractedObjects.length).toEqual(3);
+});
+
+it('should return an empty array', () => {
+  const logger = createLogger();
+
+  logger.warn('Cyber-criminals are hacking the system');
+  logger.warn('Your girlfriend tried to create a new user for Windows');
+  logger.log('You opened World of Tanks, have a great time!');
+
+  const extractedObjects = logger.getRecords('error');
+
+  expect(extractedObjects.length).toEqual(0);
+});
+
+it('should return different arrays for two loggers created with the same function', () => {
+  const logger1 = createLogger();
+  const logger2 = createLogger();
+
+  logger1.warn('Cyber-criminals are hacking the system');
+  logger2.error('Your girlfriend tried to create a new user for Windows');
+
+  expect(logger1.getRecords('error')).toEqual([]);
+  expect(logger2.getRecords('warn')).toEqual([]);
+})
