4ca61b4c2cf55821c6f53b1b5f6704975452a8fa
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2022-01-14 15:22:08 +0200

    Adding the methods startTime() and getTime() to the timer object

--- this-context/index.js
+++ this-context/index.js
@@ -62,6 +62,15 @@ const timer = {
   secondsPassed: 0,
   minsPassed: 0,
   startTimer() {
+      setInterval(() => {
+      const resetSecAddMin = () => { this.secondsPassed = 0; this.minsPassed += 1 };
+      this.secondsPassed < 60 ? this.secondsPassed += 1 : resetSecAddMin();
+      }, [1000]);
+  },
+  getTime() {
+    return this.secondsPassed < 10 ? `${this.minsPassed}:0${this.secondsPassed}` : `${this.minsPassed}:${this.secondsPassed}`;
+  },
+  stopTimer() {
 
   },
 };
